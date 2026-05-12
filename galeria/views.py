34b730c746187daa99cd6cffe2d1323575ddf695
Westes Mesquita da Silva <westesmso@PREFEITURA.SP.GOV.BR>
2026-05-12 10:59:38 -0300

    django_orm

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render (request, 'galeria/index.html')
+    dados = {
+        1: {"nome": 'Nebulosa de Carina', 'legenda': 'webbtelescope.org / NASA / James Webb'},
+        2: {'nome': 'Galáxia NGC 1079', 'legenda': 'nasa.org/ NASA / Hubble'},
+    }
+    
+    return render (request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render (request, 'galeria/imagem.html')
