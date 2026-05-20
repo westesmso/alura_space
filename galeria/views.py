9fc42dcf823d98a40f76af45799e8b02aece7390
Westes Mesquita da Silva <westesmso@PREFEITURA.SP.GOV.BR>
2026-05-20 11:57:20 -0300

    feat: implementar funcionalidade de busca e criar template para página de busca

--- galeria/views.py
+++ galeria/views.py
@@ -12,3 +12,13 @@ def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id)
     
     return render (request, 'galeria/imagem.html', {'foto': foto})
+
+def buscar(request):
+    fotografias = Fotografia.objects.filter(publicada=True).order_by('data_fotografia')
+    
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render (request, 'galeria/buscar.html', {'cards': fotografias})
