8597e3b379d261480e094dc27e0147deba0ba61b
Westes Mesquita da Silva <westesmso@PREFEITURA.SP.GOV.BR>
2026-06-12 10:03:50 -0300

    ajuste nos formulários de cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -27,16 +27,16 @@ class LoginForms(forms.Form):
     )
     
 class CadastroForms(forms.Form):
-    nome_login=forms.CharField(
-        label='Nome de Login',
-        required=True,
+    nome_cadastro=forms.CharField(
+        label='Nome de Cadastro', 
+        required=True, 
         max_length=100,
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Digite seu login'
+                'placeholder': 'Ex.: João Silva',
             }
-        ),
+        )
     )
     email=forms.EmailField(
         label='Email',
@@ -45,33 +45,31 @@ class CadastroForms(forms.Form):
         widget=forms.EmailInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Digite seu email'
+                'placeholder': 'Ex.: joaosilva@xpto.com',
+            }
+        )
+    )
+    
+    senha_1=forms.CharField(
+        label='Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
             }
         ),
     )
     
-    senha=forms.CharField(
-        label='Senha',
-        required=True,
-        min_length=6,
+    senha_2=forms.CharField(
+        label='Senha', 
+        required=True, 
         max_length=70,
         widget=forms.PasswordInput(
             attrs={
-                'class': 'form-control col-6 col-lg-6',
-                'placeholder': 'Digite sua senha'
-            },
-            ),
-    )
-    
-    confirmacao_senha=forms.CharField(
-        label='Confirmação de Senha',
-        required=True,
-        min_length=6,
-        max_length=70,
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control col-6 col-lg-6',
-                'placeholder': 'Digite sua senha'
-            },
-            ),
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente',
+            }
+        ),
     )
