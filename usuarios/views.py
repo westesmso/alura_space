2c87f9b168a642021b2bf23da2a67a99c0d1c16d
Westes Mesquita da Silva <westesmso@PREFEITURA.SP.GOV.BR>
2026-06-12 10:29:20 -0300

    lógica de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForms()
@@ -8,6 +9,29 @@ def login(request):
 
 def register(request):
     form = CadastroForms()
+    
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        
+        if form.is_valid():
+            if form["senha1"].value() != form["senha2"].value():
+                return redirect('register')
+            
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["senha1"].value()
+            
+            if User.objects.filter(username=nome).exists():
+                return redirect('register')
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
+    
     return render(request, 'users/register.html', {'form': form})
 
 def logout(request):
